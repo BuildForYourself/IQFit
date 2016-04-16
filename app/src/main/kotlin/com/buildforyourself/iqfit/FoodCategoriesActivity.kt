f4eea6cd0e33305e621714720ac1f8b411267105
Alexander <alexanderlesnyakov@gmail.com>
2016-04-16 16:10:18 +0300

    Filled FoodCategoriesActivity

--- app/src/main/kotlin/com/buildforyourself/iqfit/FoodCategoriesActivity.kt
+++ app/src/main/kotlin/com/buildforyourself/iqfit/FoodCategoriesActivity.kt
@@ -2,10 +2,63 @@ package com.buildforyourself.iqfit
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.buildforyourself.iqfit.data.FakeDataProvider
+import com.buildforyourself.iqfit.data.IDataProvider
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.toolbar
 
 class FoodCategoriesActivity : AppCompatActivity() {
+    var dataProvider : IDataProvider = FakeDataProvider()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val categories = dataProvider.loadFoodCategories()
+
+        verticalLayout(){
+            setSupportActionBar(toolbar {
+                backgroundColor = R.attr.colorPrimary
+                popupTheme = R.style.AppTheme_PopupOverlay
+            }.lparams {
+                width=matchParent
+                height= R.attr.actionBarSize
+            })
+
+            scrollView() {
+                verticalLayout() {
+                    var i: Int = 0
+                    while (i < categories.count()) {
+                        linearLayout() {
+                            orientation = LinearLayout.HORIZONTAL
+                            weightSum = 2f
+                            for (j in 0..1) {
+                                if (i == categories.count())
+                                    break
+
+                                val c = categories[i]
+                                button() {
+                                    setCompoundDrawablesWithIntrinsicBounds(null, c.icon, null, null)
+                                    text = c.name
+                                    textSize = 14f
+                                    onClick {
+                                        if (c.components != null && c.components.count() > 0)
+                                            startActivity<FoodComponentsActivity>("categoryId" to 0)
+                                    }
+                                    onLongClick {
+                                        toast("Значение по умолчанию добавлено")
+                                        true
+                                    }
+                                }.lparams {
+                                    weight = 1f
+                                    width = 0
+                                }
+                                i++
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/kotlin/com/buildforyourself/iqfit/data/FakeDataProvider.kt
+++ app/src/main/kotlin/com/buildforyourself/iqfit/data/FakeDataProvider.kt
@@ -10,15 +10,15 @@ import com.buildforyourself.iqfit.model.FoodComponent
  * Created by Ilya on 16.04.2016.
  */
 
-class FakeDataProvider {
-    fun loadFoodCategories() : List<FoodCategory> {
+class FakeDataProvider : IDataProvider {
+    override fun loadFoodCategories() : List<FoodCategory> {
 //        var foodCategories: MutableList<FoodCategory> = mutableListOf<FoodCategory>()
 //        for (i : Int in 1..12) {
 //            val foodCategory = FoodCategory ("Суп{i}", BitmapDrawable(), i,  true, listOf<FoodComponent>())
 //            foodCategories.add(foodCategory);
 //        }
 
-        var foodCategories = listOf<FoodCategory> (
+        var foodCategories = listOf (
                 FoodCategory ("Супы", BitmapDrawable(), 1,  true, listOf<FoodComponent>()),
                 FoodCategory ("Салаты", BitmapDrawable(), 2,  true, listOf<FoodComponent>()),
                 FoodCategory ("Сладости", BitmapDrawable(), 3,  true, listOf<FoodComponent>()),
