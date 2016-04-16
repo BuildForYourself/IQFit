cffe0fd2a4b4dd1b7f820134be77d03099a6363e
Alexander <alexanderlesnyakov@gmail.com>
2016-04-16 15:03:49 +0300

    Added FoodCategoriesActivity and FoodComponentsActivity

---
+++ app/src/main/kotlin/com/buildforyourself/iqfit/FoodCategoriesActivity.kt
@@ -0,0 +1,11 @@
+package com.buildforyourself.iqfit
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FoodCategoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/kotlin/com/buildforyourself/iqfit/StackActivity.kt
+++ app/src/main/kotlin/com/buildforyourself/iqfit/StackActivity.kt
@@ -2,8 +2,6 @@ package com.buildforyourself.iqfit
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
@@ -13,6 +11,7 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import org.jetbrains.anko.find
+import org.jetbrains.anko.*
 
 class StackActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,12 +22,14 @@ class StackActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         setSupportActionBar(toolbar)
 
         val fab = find<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener({ view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        fab.setOnClickListener {
+            startActivity<FoodCategoriesActivity>()
+        }
 
         val drawer = find<DrawerLayout>(R.id.drawer_layout)
         val toggle = ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
+        drawer.addDrawerListener(toggle)
         toggle.syncState()
 
         val navigationView = find<NavigationView>(R.id.nav_view)
