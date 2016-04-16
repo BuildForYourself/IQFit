3acc3c30cf91cfa5882a968b6529ff4dbf96d5a8
Ilia.Petukhov <ispbox@gmail.com>
2016-04-16 21:47:25 +0300

    EN: On the way to DBFlow

---
+++ app/src/main/kotlin/com/buildforyourself/iqfit/IQFitApplication.kt
@@ -0,0 +1,15 @@
+package com.buildforyourself.iqfit
+
+import android.app.*
+
+/**
+ * Created by Ilya on 16.04.2016.
+ */
+
+class IQFitApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //FlowManager.init(this)
+    }
+}
