cd9bd8029cc620fc40601275b8155da52d9a057a
Ilia.Petukhov <ispbox@gmail.com>
2016-04-16 16:04:30 +0300

    EN: Updated fake data provider.

--- app/src/main/kotlin/com/buildforyourself/iqfit/data/FakeDataProvider.kt
+++ app/src/main/kotlin/com/buildforyourself/iqfit/data/FakeDataProvider.kt
@@ -2,6 +2,7 @@ package com.buildforyourself.iqfit.data
 
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
 import com.buildforyourself.iqfit.model.FoodCategory
 import com.buildforyourself.iqfit.model.FoodComponent
 
@@ -11,9 +12,22 @@ import com.buildforyourself.iqfit.model.FoodComponent
 
 class FakeDataProvider {
     fun loadFoodCategories() : List<FoodCategory> {
+//        var foodCategories: MutableList<FoodCategory> = mutableListOf<FoodCategory>()
+//        for (i : Int in 1..12) {
+//            val foodCategory = FoodCategory ("Суп{i}", BitmapDrawable(), i,  true, listOf<FoodComponent>())
+//            foodCategories.add(foodCategory);
+//        }
 
-        var foodCategory = FoodCategory ("Суп", BitmapDrawable(), 1,  true, listOf<FoodComponent>())
+        var foodCategories = listOf<FoodCategory> (
+                FoodCategory ("Супы", BitmapDrawable(), 1,  true, listOf<FoodComponent>()),
+                FoodCategory ("Салаты", BitmapDrawable(), 2,  true, listOf<FoodComponent>()),
+                FoodCategory ("Сладости", BitmapDrawable(), 3,  true, listOf<FoodComponent>()),
+                FoodCategory ("Напитки", BitmapDrawable(), 4,  true, listOf<FoodComponent>()),
+                FoodCategory ("Фрукты", BitmapDrawable(), 5,  true, listOf<FoodComponent>()),
+                FoodCategory ("Мясо", BitmapDrawable(), 6,  true, listOf<FoodComponent>()),
+                FoodCategory ("Сыры", BitmapDrawable(), 7,  true, listOf<FoodComponent>())
+        )
 
-        return listOf<FoodCategory> (foodCategory);
+        return foodCategories;
     }
 }
