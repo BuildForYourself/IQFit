79769bd8baebc715dee1888953932914153ef38e
Ilia.Petukhov <ispbox@gmail.com>
2016-04-16 14:19:15 +0300

    EN: Added draft object model.

---
+++ app/src/main/kotlin/Model.kt
@@ -0,0 +1,56 @@
+import android.graphics.drawable.Drawable
+import java.util.*
+
+class FoodCategory (
+        val name: String,
+        val icon: Drawable,
+        var priority: Int,
+        var isActive: Boolean,
+        var components: List<FoodComponent>) {
+}
+
+open class FoodComponent (
+ val name: String,
+ val icon: Drawable,
+ val group: String,
+ val isSingle: Boolean,
+ var isDefault: Boolean,
+ var isSelected: Boolean) {
+}
+
+class CalorieComponent
+(name: String,
+ icon: Drawable,
+ group: String,
+ isSingle: Boolean,
+ isDefault: Boolean,
+ isSelected: Boolean,
+ val calories: Int,
+ val operation: Operation)
+: FoodComponent(name, icon, group, isSingle, isDefault, isSelected) {
+}
+
+class QuantityComponent
+(name: String,
+ icon: Drawable,
+ group: String,
+ isSingle: Boolean,
+ isDefault: Boolean,
+ isSelected: Boolean,
+ val multiplier: Double)
+: FoodComponent(name, icon, group, isSingle, isDefault, isSelected) {
+
+}
+
+enum class Operation {
+    Sum,
+    Avg
+}
+
+class Food
+(val foodCategory: FoodCategory,
+ val foodComponents: List<FoodComponent>,
+ val dateTime: Date,
+ val calories: Int) {
+
+}
